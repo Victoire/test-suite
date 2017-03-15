5bc705fe79ebe8c4ca85e642495c120fa54b1773
Grégoire Humeau <gregoire@appventus.com>
2017-03-15 17:41:12 +0100

    Rename Test/Functionnal folder into Test/App

--- dependencies.sh
+++ dependencies.sh
@@ -12,7 +12,7 @@ if [[ $1 == *"widget"* ]]; then
     cd vendor/victoire/victoire/
 fi
 
-cp Tests/Functionnal/app/config/parameters.yml.dist Tests/Functionnal/app/config/parameters.yml
+cp Tests/App/app/config/parameters.yml.dist Tests/App/app/config/parameters.yml
 if [ -z "${RUN_NIGHTLY_BUILD}" ]; then
   sed -i '/CoverageContext/d' behat.yml
   sed -i '/CoverageContext/d' behat.yml.dist
@@ -28,18 +28,18 @@ if [[ $1 == *"widget"* ]]; then
 fi
 
 (cd Bundle/UIBundle/Resources/config/ && bower install)
-php Tests/Functionnal/bin/console --env=ci doctrine:database:create --no-debug
-php Tests/Functionnal/bin/console --env=ci doctrine:schema:create --no-debug
-php Tests/Functionnal/bin/console --env=ci cache:warmup --no-debug
-php Tests/Functionnal/bin/console --env=domain cache:warmup --no-debug
-php Tests/Functionnal/bin/console --env=ci victoire:generate:view --no-debug
-php Tests/Functionnal/bin/console --env=ci assets:install Tests/Functionnal/web --no-debug
-php Tests/Functionnal/bin/console --env=ci bazinga:js-translation:dump --no-debug
-php Tests/Functionnal/bin/console --env=ci fos:js:dump --target="Tests/Functionnal/web/js/fos_js_routes_test.js" --no-debug
-php Tests/Functionnal/bin/console --env=domain fos:js:dump --target="Tests/Functionnal/web/js/fos_js_routes_domain.js" --no-debug
-php Tests/Functionnal/bin/console --env=ci assetic:dump --no-debug
+php Tests/App/bin/console --env=ci doctrine:database:create --no-debug
+php Tests/App/bin/console --env=ci doctrine:schema:create --no-debug
+php Tests/App/bin/console --env=ci cache:warmup --no-debug
+php Tests/App/bin/console --env=domain cache:warmup --no-debug
+php Tests/App/bin/console --env=ci victoire:generate:view --no-debug
+php Tests/App/bin/console --env=ci assets:install Tests/App/web --no-debug
+php Tests/App/bin/console --env=ci bazinga:js-translation:dump --no-debug
+php Tests/App/bin/console --env=ci fos:js:dump --target="Tests/App/web/js/fos_js_routes_test.js" --no-debug
+php Tests/App/bin/console --env=domain fos:js:dump --target="Tests/App/web/js/fos_js_routes_domain.js" --no-debug
+php Tests/App/bin/console --env=ci assetic:dump --no-debug
 wget http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.1.jar
 nohup java -jar selenium-server-standalone-2.53.1.jar > /dev/null 2>&1 &
-php Tests/Functionnal/bin/console --env=ci server:start 127.0.0.1:8000 -r Tests/Functionnal/app/config/router_ci.php --no-debug > server.log 2>&1
+php Tests/App/bin/console --env=ci server:start 127.0.0.1:8000 -r Tests/App/app/config/router_ci.php --no-debug > server.log 2>&1
 nohup Xvfb :99 -ac 2>/dev/null &
 sleep 3
