03bfce174bf408385a078fa86cd60bfa03b40d38
Paul Andrieux <paul@troopers.email>
2017-06-23 15:31:19 +0200

    scenario parralelization

--- circle.sh
+++ circle.sh
@@ -24,43 +24,49 @@ if [ -z "${RUN_NIGHTLY_BUILD}" ]; then
     echo "\$CIRCLE_NODE_TOTAL = $CIRCLE_NODE_TOTAL"
     echo "\$CIRCLE_NODE_INDEX = $CIRCLE_NODE_INDEX"
 
-    error=0
-    pwd=$(pwd)
-    declare -a files
-    declare -a params
-    # Fetch all of the feature files for each parameter (directories)
+    # Don't rely on default order
+    # http://serverfault.com/questions/181787/find-command-default-sorting-order/181815#181815
+    availableScenarios=($(./vendor/bin/behat $path --dry-run | grep "Scenario: " | awk '{print $(NF)}' > var/scenarios))
+    let "scenarioCount = ${#availableScenarios[@]}"
+    echo "$scenarioCount features found"
 
-    echo "Seaching $path for feature files..."
-    if [ -d "$path" ]; then
-      while read -r entry; do
-        echo "-- found $entry"
-        files=(${files[@]} "$entry")
-        # Bash wizardry so we can update the array (otherwise it's a subprocess)
-      done < <(find $path | grep "\.feature$")
-    else
-      # Fetch all the params to be passed later to Behat
-      # Quote named params to allow special chars inside
-      # them. This allow us to use && || or any other
-      # reserved operand inside params.
-      if [[ $path == *"="* ]]
-      then
-        arrParam=(${param//=/ })
-        param="${arrParam[0]}='${arrParam[1]}'"
-      fi
-      echo "Fetch param $path"
-      params=(${params[@]} "$path")
+    let "scenariosToRunCount = scenarioCount / $CIRCLE_NODE_TOTAL"
+    let "modulo = scenarioCount % $CIRCLE_NODE_TOTAL"
+    if [ "$modulo" -ne 0 ]; then
+        let "scenariosToRunCount += 1"
     fi
+    fromScenarioIndex=1
 
-    for i in "${!files[@]}"; do
-        if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]; then
-          echo "Running ./vendor/bin/behat --format=pretty --out=std --format=junit --out=$CIRCLE_TEST_REPORTS/$i/junit --rerun="return.log" ${params} `pwd`/${files[$i]}"
-          time ./vendor/bin/behat --format=pretty --out=std --format=junit --out=$CIRCLE_TEST_REPORTS/$i/junit ${params} `pwd`/${files[$i]}
-        fi
-        # Mark the entire script as a failure if any of the iterations fail.
-        if [ ! $? -eq 0 ]
-        then
-          error=1
+    let "toScenarioIndex = (scenariosToRunCount) * $CIRCLE_NODE_INDEX - 1"
+    let "fromScenarioIndex = toScenarioIndex - scenariosToRunCount + 1"
+    echo "$scenariosToRunCount features to run here [$fromScenarioIndex-$toScenarioIndex]"
+
+    echo "scenarios:"
+
+    scenariosToRun=()
+    for ((i=0; i < ${#availableScenarios[@]}; i++)); do
+        if [ "$i" -ge $fromScenarioIndex ] && [ "$i" -le $toScenarioIndex ]; then
+            echo "- ${availableScenarios[$i]}"
+            scenariosToRun+=(${availableScenarios[$i]})
         fi
     done
-    exit $error
+
+    # Calculate sum of return codes in order to detect errors
+    # http://stackoverflow.com/questions/6348902/how-can-i-add-numbers-in-a-bash-script/6348945#6348945
+    sum=0
+    if [ "${#scenariosToRun[@]}" -gt 0 ]; then
+        for ((i=0; i < ${#scenariosToRun[@]}; i++)); do
+            echo "bin/behat -vv ${scenariosToRun[$i]}"
+            time bin/behat -vv ${scenariosToRun[$i]}
+            echo "Running ./vendor/bin/behat --format=pretty --out=std --format=junit --out=$CIRCLE_TEST_REPORTS/$i/junit --rerun="return.log" ${scenariosToRun[$i]}"
+            time ./vendor/bin/behat --format=pretty --out=std --format=junit --out=$CIRCLE_TEST_REPORTS/$i/junit ${scenariosToRun[$i]}
+            return=$?
+            echo "return code = ${return}"
+            sum=$(( $sum + $return ))
+        done
+    else
+        echo "No test to run (issue related to modulo)"
+    fi
+    echo "sum of Behat return codes = ${sum}"
+    exit $sum
 fi
